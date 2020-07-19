5adcb8d0fe09e2dbf98a8aaec27f1107f2edc79c
Diego Viniegra <diego.viniegra@gmail.com>
2020-07-18 22:33:09 -0500

    WIP canvasjs

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
         </span>
       </div>
       <div class="field is-grouped">
+        <button @click="graph" type="button" class="control button is-info">
+          graph
+        </button>
         <button @click="connect" type="button" class="button control">
           connect
         </button>
@@ -43,14 +46,22 @@
         </table>
       </div>
     </section>
+    <section>
+      <div id="chart1">
+
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+// import CanvasJS from 'canvasjs';
 import google from '@/services/google'
 import parseImagesMixin from '@/mixins/parseImages'
 import { mapDeltas } from '@/analytics'
 import { inspect } from '@/functions'
+
+// var CanvasJS = require('canvasjss')
 
 export default {
   name: 'App',
@@ -84,6 +95,77 @@ export default {
       console.log(fs)
 
       this.files = fs
+    },
+
+    graph() {
+      var chart = new CanvasJS.Chart('chart1', {
+        animationEnabled: true,
+        title:{
+          text: "Daily High Temperature at Different Beaches"
+        },
+        axisX: {
+          valueFormatString: "DD MMM,YY"
+        },
+        axisY: {
+          title: "Temperature (in °C)",
+          includeZero: false,
+          suffix: " °C"
+        },
+        legend:{
+          cursor: "pointer",
+          fontSize: 16,
+          // itemclick: toggleDataSeries
+        },
+        toolTip:{
+          shared: true
+        },
+        data: [{
+          name: "Myrtle Beach",
+          type: "spline",
+          yValueFormatString: "#0.## °C",
+          showInLegend: true,
+          dataPoints: [
+            { x: new Date(2017,6,24), y: 31 },
+            { x: new Date(2017,6,25), y: 31 },
+            { x: new Date(2017,6,26), y: 29 },
+            { x: new Date(2017,6,27), y: 29 },
+            { x: new Date(2017,6,28), y: 31 },
+            { x: new Date(2017,6,29), y: 30 },
+            { x: new Date(2017,6,30), y: 29 }
+          ]
+        },
+        {
+          name: "Martha Vineyard",
+          type: "spline",
+          yValueFormatString: "#0.## °C",
+          showInLegend: true,
+          dataPoints: [
+            { x: new Date(2017,6,24), y: 20 },
+            { x: new Date(2017,6,25), y: 20 },
+            { x: new Date(2017,6,26), y: 25 },
+            { x: new Date(2017,6,27), y: 25 },
+            { x: new Date(2017,6,28), y: 25 },
+            { x: new Date(2017,6,29), y: 25 },
+            { x: new Date(2017,6,30), y: 25 }
+          ]
+        },
+        {
+          name: "Nantucket",
+          type: "spline",
+          yValueFormatString: "#0.## °C",
+          showInLegend: true,
+          dataPoints: [
+            { x: new Date(2017,6,24), y: 22 },
+            { x: new Date(2017,6,25), y: 19 },
+            { x: new Date(2017,6,26), y: 23 },
+            { x: new Date(2017,6,27), y: 24 },
+            { x: new Date(2017,6,28), y: 24 },
+            { x: new Date(2017,6,29), y: 23 },
+            { x: new Date(2017,6,30), y: 23 }
+          ]
+        }]
+      });
+      chart.render();
     }
   },
 
